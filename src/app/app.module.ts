3ecb9999112ac4fea102defeb5088780b8e4ec9d
Anthony Vargas <thonyalope@gmail.com>
2025-07-22 23:51:51 -0600

    TS-01: Create base RiskCatalog page with translated tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app.material.module';
-import { HttpClient } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader { return new TranslateHttpLoader(http, './assets/i18n/', '.json'); }
+import { RiskCatalogComponent } from './pages/risk-catalog/risk-catalog.component';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RiskCatalogComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AppMaterialModule,
     BrowserAnimationsModule,
     TranslateModule.forRoot({
+      defaultLanguage: 'es',
       loader: {
-      provide: TranslateLoader,
-      useFactory: (HttpLoaderFactory),
-      deps: [HttpClient]
-    }}),
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
